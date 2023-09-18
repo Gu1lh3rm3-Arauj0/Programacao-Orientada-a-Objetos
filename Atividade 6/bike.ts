3e59c67b7726561a1ab7f9ac4514df9f0e9e6407
Guilherme Araújo <guilherme.a.m.souza0613@gmail.com>
2023-09-17 22:57:40 -0300

    Add files via upload
    Localização da bike

---
+++ Atividade 6/bike.ts
@@ -0,0 +1,20 @@
+export class Bike {
+  constructor(
+      public name: string,
+      public type: string,
+      public bodySize: number,
+      public maxLoad: number,
+      public rate: number,
+      public description: string,
+      public ratings: number,
+      public imageUrls: string[],
+      public Location: {
+        latitude: number;
+        longitude: number;
+        city: string;
+        state: string;
+      },
+      public available: boolean = true,
+      public id?: string
+  ) {}
+}
